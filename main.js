e906cf15a0b59be8cac9960c1b2df4ee7690554c
github username <github email address>
2021-09-18 14:11:43 -0500

    commit tree 382b1bbc51fb1eaffba477654285bbaef69127a2

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphStates = {
+  '♡': '♥',
+   '♥': '♡' };
 
-
+      const colorStates = {
+        "red": "", 
+        "": "red"};
+      const articleHearts = document.querySelectorAll(".like-glyph");
+      function likeCallback(e) {
+        const heart = e.target;
+        mimicServerCall("boqusUrl")
+        .then(function(serverMessage){
+          heart.innerText = glyphStates[heart.innerText];
+          heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function(error){
+          const modal = document.getElementById("modal");
+          modal.className = "";
+          modal.innerText = error;
+          setTimeout(() => modal.className = "hidden", 3000);
+        });
+      }          
+ for (const glyph of articleHearts) {
+   glyph.addEventListener("click", likeCallback);
+ }
 
 
 //------------------------------------------------------------------------------
